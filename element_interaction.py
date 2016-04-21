3cc90cd504887eeda7cbf013c066eafa5cb80a20
weyenk <noahkrus@gmail.com>
2016-04-21 06:31:42 -0400

    Added additional output to select option

--- element_interaction.py
+++ element_interaction.py
@@ -54,25 +54,25 @@ class ElementInteraction(ElementIdentification):
             selection = Select(element)
             if selection_type.lower() == "index":
                 selection.select_by_index(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                # print("Select '" + + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower() == "text":
                 selection.select_by_visible_text(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                print("Select '" + value + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower() == "value":
                 selection.select_by_value(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                # print("Select '" + + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower() == "deindex":
                 selection.deselect_by_index(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                # print("Deselect '" + + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower() == "detext":
                 selection.deselect_by_visible_text(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                print("Deselect '" + value + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower() == "devalue":
                 selection.deselect_by_value(value)
-                # print("Select '" + + "' from the '" + + "' dropdown")
+                # print("Deselect '" + + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             elif selection_type.lower == "deall":
                 selection.deselect_all()
-                # print("Deselected all values from the '" + + "' dropdown")
+                # print("Deselected all values from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
             else:
                 raise Exception("Unknown selection type.  Attach name:" + attach_name)
 
