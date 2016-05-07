9c6e1af4b02646e1a0a145e9c46d1cd078f72ddd
weyenk <noahkrus@gmail.com>
2016-05-07 08:18:05 -0400

    Added visibility check

--- element_interaction.py
+++ element_interaction.py
@@ -17,16 +17,24 @@ class ElementInteraction(ElementIdentification):
         element = self.ei.uniquely_identify_element(param_array)
 
         if element is not None:
-            print("Click the '" + element.text + "' " + self.ei.get_element_type(element))
-            element.click()
+            if element.is_displayed():
+                print("Click the '" + element.text + "' " + self.ei.get_element_type(element))
+                element.click()
+            else:
+                raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
 
     def enter_text(self, param_array, text_value):
 
         element = self.ei.uniquely_identify_element(param_array)
 
         if element is not None:
-            print("Enter '" + str(text_value) + "' in the '" + str(self.find_html_for(element.get_attribute('id'))) + "' textbox")
-            element.send_keys(text_value)
+            # Determine if element is visible
+            if element.is_displayed():
+                print("Enter '" + str(text_value) + "' in the '" + str(self.find_html_for(element.get_attribute('id'))) + "' textbox")
+                element.send_keys(text_value)
+            else:
+                # This should really check if the element has an ID before assuming.  Perhaps make a method to check
+                raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
 
     def select_option(self, param_array, selection_value, selection_type, multiline=None):
 
@@ -35,33 +43,47 @@ class ElementInteraction(ElementIdentification):
         if element is not None:
             selection = Select(element)
             if selection_type.lower() == "index":
-                selection.select_by_index(selection_value)
-                print("Select '" + selection.all_selected_options + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
+                if element.is_displayed():
+                    selection.select_by_index(selection_value)
+                    print("Select '" + selection.all_selected_options + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower() == "text":
-                selection.select_by_visible_text(selection_value)
-                print("Select '" + selection_value + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
+                if element.is_displayed():
+                    selection.select_by_visible_text(selection_value)
+                    print("Select '" + selection_value + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower() == "value":
-                selection.select_by_value(selection_value)
-                print("Select '" + selection.all_selected_options + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
+                if element.is_displayed():
+                    selection.select_by_value(selection_value)
+                    print("Select '" + selection.all_selected_options + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower() == "deindex":
-                print("Deselect '" + selection.all_selected_options + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
-                selection.deselect_by_index(selection_value)
+                if element.is_displayed():
+                    print("Deselect '" + selection.all_selected_options + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                    selection.deselect_by_index(selection_value)
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower() == "detext":
-                print("Deselect '" + selection_value + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
-                selection.deselect_by_visible_text(selection_value)
+                if element.is_displayed():
+                    print("Deselect '" + selection_value + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                    selection.deselect_by_visible_text(selection_value)
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower() == "devalue":
-                print("Deselect '" + selection.all_selected_options + "' from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
-                selection.deselect_by_value(selection_value)
+                if element.is_displayed():
+                    print("Deselect '" + selection.all_selected_options + "' from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                    selection.deselect_by_value(selection_value)
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             elif selection_type.lower == "deall":
-                selection.deselect_all()
-                print("Deselected all values from the '" + self.find_html_for(
-                    element.get_attribute('id')) + "' dropdown")
+                if element.is_displayed():
+                    selection.deselect_all()
+                    print("Deselected all values from the '" + self.find_html_for(element.get_attribute('id')) + "' dropdown")
+                else:
+                    raise Exception('Element is present but not visible. ID: ' + element.get_attribute('id'))
             else:
                 # This should really just be reported and not raise an exception
                 raise Exception("Unknown selection type.")
