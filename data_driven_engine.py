13b1f71b7dd4ca63e6358178cde0c51da403033a
weyenk <noahkrus@gmail.com>
2016-05-11 17:48:10 -0400

    Added email functionality and improved documentation

--- data_driven_engine.py
+++ data_driven_engine.py
@@ -4,7 +4,8 @@ from element_interaction import ElementInteraction
 from selenium import webdriver
 import os
 import time
-import datetime
+import smtplib
+from email.mime.text import MIMEText
 
 class DataDrivenEngine(ElementInteraction):
 
@@ -16,9 +17,17 @@ class DataDrivenEngine(ElementInteraction):
         self.json_obj = json.loads(raw_file_data)
 
     def __parse_data(self, to_be_parsed):
-        results = re.findall("'(\w+.*?)'", to_be_parsed)
+        results = []
+
+        tmp_list = list(to_be_parsed.items())
+        dict_count = len(tmp_list)
+        i = 0
+        while i < dict_count:
+            results.append(str(tmp_list[i][0]))
+            results.append(str(tmp_list[i][1]))
+            i += 1
+
         if results:
-            # print(results)
             return results
         else:
             return None
@@ -95,8 +104,6 @@ class DataDrivenEngine(ElementInteraction):
 
     def __run_sites(self, counter, driver, output):
         driver.get(self.json_obj['sites'][counter]['url'])
-        if driver.name == 'MicrosoftEdge':
-            time.sleep(3)
         self.__report_step(('Navigate to ' + self.json_obj['sites'][counter]['url']), output)
 
     def __run_data(self, ei, output):
@@ -105,14 +112,16 @@ class DataDrivenEngine(ElementInteraction):
         i = 0
         while i < len(self.json_obj['data']):
             for obj in self.json_obj['data']:
-                if obj['order'] == i + 1:
-                    results = self.__parse_data(str(obj['identifier']))
+                if obj['order'] == i + 1: # Increase i by 1 to make json file human readable
+                    results = self.__parse_data(obj['identifier'])
                     if obj['action'] == "click object":
-                        action = ei.click_object(results)
+                        action = ei.click_object(results, obj['child window'])
                     elif obj['action'] == "enter text":
-                        action = ei.enter_text(results, obj['text value'])
+                        action = ei.enter_text(results, obj['text value'], obj['child window'])
                     elif obj['action'] == "select option":
-                        action = ei.select_option(results, obj['selection value'], obj['selection type'])
+                        action = ei.select_option(results, obj['selection value'], obj['selection type'], obj['child window'])
+                    elif obj['action'] == "handle alert":
+                        action = ei.handle_alert(obj['alert action'], obj['child window'])
                     else:
                         raise Exception("Step " + obj['order'] + " does not have a known action type.")
             i += 1
@@ -131,17 +140,30 @@ class DataDrivenEngine(ElementInteraction):
     def __report_step(self, action_to_report, output):
         output.write('\n' + action_to_report)
 
-    def __email(self, email_address, email_subject, email_body, email_signature=None):
-        # Determine if signature is required
-        # Send email
-        pass
+    def __email(self, email_address, email_subject, email_body, attachments=None, email_signature=None):
+        ''' This function still needs code to handle attachments and an email signature'''
+
+        message = MIMEText(email_body)
+        message['Subject'] = email_subject
+        message['From'] = 'zoebear@geosolinc.com'
+        match = re.search(".*?\w+@\w+\.\w+$",email_address)
+        if match:
+            message['To'] = email_address
+        else:
+            raise Exception('Email address is not valid')
+        mail = smtplib.SMTP('mail_internal.geosolinc.com')
+        mail.send_message(message)
+        mail.quit()
 
     def __quit(self, start_time, output):
         email_address = self.json_obj['options']['email']
-        stop_time = time.time()
-        unix_runtime = stop_time - start_time
-        runtime = datetime.datetime.fromtimestamp(unix_runtime).strftime('%Y-%m-%d %H:%M:%S')
+        stop_time = time.struct_time.tm_min
+        unix_runtime = time.time() - start_time
+        min, sec = divmod(unix_runtime, 60)
+        hour, min = divmod(min, 60)
+        runtime = '%d:%02d:%02d' % (hour, min, sec)
+        output.write('\n\nTotal Runtime: ' + runtime)
         output.close()
         if email_address is not None:
-            self.__email(email_address, self.json_obj['options']['script name'], ())
+            self.__email(email_address, self.json_obj['options']['script name'] + ' has completed.', self.json_obj['options']['script name'] + ' - Runtime: ' + runtime )
         self.driver.quit()
